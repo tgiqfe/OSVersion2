fddfc3a39885e3c949ab4d121b060a0a2e379316
tgiqfe <tqji31001@gmail.com>
2021-11-27 15:57:33 +0900

    Add FindWindows class.

---
+++ OSVersion2/Windows/OSVersion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+
+namespace OSVersion2.Windows
+{
+    internal class OSVersion
+    {
+        public static void GetCurrent()
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                GetWindows();
+            }
+            else if (OperatingSystem.IsMacOS())
+            {
+                GetMacOS();
+            }
+            else if (OperatingSystem.IsLinux())
+            {
+                GetLinux();
+            }
+        }
+
+        /// <summary>
+        /// Windows用のバージョン取得
+        /// </summary>
+        public static void GetWindows()
+        {
+
+        }
+
+        //  Mac用のバージョン取得。後回し
+        public static void GetMacOS() { }
+
+        //  Linux用のバージョン取得。後回し
+        public static void GetLinux() { }
+    }
+}
